8d24e0e3b373dbf7edbdaae7a6b69428c8ec8595
festy1986 <jasonlives1986@yahoo.com>
2025-09-19 21:36:01 -0400

    Create debug_epg.js

---
+++ scripts/debug_epg.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const zlib = require('zlib');
+const xml2js = require('xml2js');
+
+(async () => {
+  try {
+    const data = zlib.gunzipSync(fs.readFileSync('./public/epg6_modified.xml.gz'));
+    const result = await xml2js.parseStringPromise(data);
+
+    const programmes = result.tv.programme.map(p => ({
+      channel: p.$?.channel,
+      title: p.title?.[0]?._ || p.title?.[0] || '',
+      desc: p.desc?.[0]?._ || p.desc?.[0] || '',
+      start: p.$?.start,
+      stop: p.$?.stop
+    }));
+
+    console.log(JSON.stringify(programmes, null, 2));
+  } catch (err) {
+    console.error('Debug script error:', err);
+  }
+})();
