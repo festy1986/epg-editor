f309500740213b12a9e180cb7b1db1940c81f4f1
festy1986 <jasonlives1986@yahoo.com>
2025-09-19 21:38:52 -0400

    Update debug_epg.js

--- scripts/debug_epg.js
+++ scripts/debug_epg.js
@@ -7,6 +7,7 @@ const xml2js = require('xml2js');
     const data = zlib.gunzipSync(fs.readFileSync('./public/epg6_modified.xml.gz'));
     const result = await xml2js.parseStringPromise(data);
 
+    // Map all programmes for inspection
     const programmes = result.tv.programme.map(p => ({
       channel: p.$?.channel,
       title: p.title?.[0]?._ || p.title?.[0] || '',
